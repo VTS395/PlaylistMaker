00eff854c76f284fe6ff82afd7de629c9affd685
VTS395 <tsoy@gmlab.pro>
2024-10-19 23:22:50 +0300

    gaps for buttons in main activity

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.searchButton)
+        val mediaButton = findViewById<Button>(R.id.mediaButton)
+        val settingsButton = findViewById<Button>(R.id.settingsButton)
+
+        searchButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Заглушка кнопки поиска", Toast.LENGTH_SHORT).show()
         }
+
+        mediaButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Заглушка кнопки медиа", Toast.LENGTH_SHORT).show()
+        }
+
+        val settingsClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Заглушка для кнопки настроек", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        settingsButton.setOnClickListener(settingsClickListener)
     }
 }
